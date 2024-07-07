df7fd15a9777ab2bb2b0fdc29c43e55246edef94
babyo77 <onlybabyyo7@gmail.com>
2024-07-07 14:58:10 +0530

    Update interests.tsx

--- src/components/interests.tsx
+++ src/components/interests.tsx
@@ -20,6 +20,17 @@ import { motion } from "framer-motion";
 import { confettiAnimation } from "./ui/confettiAnimation";
 import { Loader } from "lucide-react";
 import TagsInput from "./ui/Addtags";
+import { useMediaQuery } from "@react-hook/media-query";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
 interface search extends Models.Document {
   text: string;
 }
@@ -119,37 +130,40 @@ function Interests({
   const handleKeywordsChange = (newKeywords: string[]) => {
     setInterest(newKeywords);
   };
-  return (
-    <AlertDialog defaultOpen={isOpen}>
-      <AlertDialogTrigger
-        className={className?.replace("hidden", "")}
-        style={{
-          visibility: className == "hidden" ? "hidden" : "visible",
-          position: className == "hidden" ? "absolute" : "static",
-        }}
-      >
-        <TiPointOfInterest className="h-7 w-7" />
-      </AlertDialogTrigger>
-      <AlertDialogContent className="w-[90dvw] rounded-xl">
-        <AlertDialogHeader>
-          <AlertDialogTitle>Add Interests</AlertDialogTitle>
-          <AlertDialogDescription>
-            This will be used to discover people with similar interests.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <motion.div
-          initial={{ filter: "blur(10px)", opacity: 0 }}
-          animate={{ filter: "blur(0px)", opacity: 1 }}
-          transition={{ duration: 0.4 }}
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <AlertDialog defaultOpen={isOpen}>
+        <AlertDialogTrigger
+          className={className?.replace("hidden", "")}
+          style={{
+            visibility: className == "hidden" ? "hidden" : "visible",
+            position: className == "hidden" ? "absolute" : "static",
+          }}
         >
-          <div className=" relative mb-3">
-            <TagsInput
-              // onChange={handleSearch}
-              initialKeywords={interested}
-              onKeywordsChange={handleKeywordsChange}
-              placeholder="Type interest and hit enter"
-            />
-            {/* {defaultInterests && defaultInterests.length > 0 && (
+          <TiPointOfInterest className="h-7 w-7" />
+        </AlertDialogTrigger>
+        <AlertDialogContent className="w-[90dvw] rounded-xl">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Add Interests</AlertDialogTitle>
+            <AlertDialogDescription>
+              This will be used to discover people with similar interests.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <motion.div
+            initial={{ filter: "blur(10px)", opacity: 0 }}
+            animate={{ filter: "blur(0px)", opacity: 1 }}
+            transition={{ duration: 0.4 }}
+          >
+            <div className=" relative mb-3">
+              <TagsInput
+                // onChange={handleSearch}
+                initialKeywords={interested}
+                onKeywordsChange={handleKeywordsChange}
+                placeholder="Type interest and hit enter"
+              />
+              {/* {defaultInterests && defaultInterests.length > 0 && (
               <div className=" absolute bg-zinc-900/95 border-2 max-h-32 overflow-x-hidden  overflow-y-scroll no-scrollbar w-full z-10 text-sm rounded-xl mt-1 flex flex-col">
                 <>
                   {defaultInterests.map(({ text }, i) => (
@@ -166,7 +180,7 @@ function Interests({
                 </>
               </div>
             )} */}
-            {/* <div className=" flex bottom-1.5 right-1 absolute gap-1">
+              {/* <div className=" flex bottom-1.5 right-1 absolute gap-1">
               <Button
                 onClick={() => setInterest([])}
                 size={"sm"}
@@ -185,28 +199,119 @@ function Interests({
                 Add
               </Button>
             </div> */}
-          </div>
-        </motion.div>
-        <AlertDialogFooter>
-          <AlertDialogCancel
-            ref={CloseRef}
-            style={{ visibility: "hidden", position: "absolute" }}
-          ></AlertDialogCancel>
-          <AlertDialogCancel
-            disabled={interested.length <= 1}
-            className={` bg-zinc-900 outline-none ring-0  rounded-xl hover:bg-zinc-800 ${className} text-white`}
+            </div>
+          </motion.div>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              ref={CloseRef}
+              style={{ visibility: "hidden", position: "absolute" }}
+            ></AlertDialogCancel>
+            <AlertDialogCancel
+              disabled={interested.length <= 1}
+              className={` bg-zinc-900 outline-none ring-0  rounded-xl hover:bg-zinc-800 ${className} text-white`}
+            >
+              Cancel
+            </AlertDialogCancel>
+            <Button
+              className=" bg-zinc-900 border rounded-xl text-white hover:bg-zinc-800"
+              onClick={handleContinue}
+            >
+              {loader ? <Loader className=" animate-spin h-5 w-5" /> : "Save"}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger
+        className={className?.replace("hidden", "")}
+        style={{
+          visibility: className == "hidden" ? "hidden" : "visible",
+          position: className == "hidden" ? "absolute" : "static",
+        }}
+      >
+        <TiPointOfInterest className="h-7 w-7" />
+      </DrawerTrigger>
+      <DrawerContent className=" border-none">
+        <DrawerHeader>
+          <DrawerTitle>Add Interests</DrawerTitle>
+          <DrawerDescription>
+            This will be used to discover people with similar interests.
+          </DrawerDescription>
+
+          <motion.div
+            initial={{ filter: "blur(10px)", opacity: 0 }}
+            animate={{ filter: "blur(0px)", opacity: 1 }}
+            transition={{ duration: 0.4 }}
           >
-            Cancel
-          </AlertDialogCancel>
+            <div className=" relative mb-3">
+              <TagsInput
+                // onChange={handleSearch}
+                initialKeywords={interested}
+                onKeywordsChange={handleKeywordsChange}
+                placeholder="Type interest and hit enter"
+              />
+              {/* {defaultInterests && defaultInterests.length > 0 && (
+            <div className=" absolute bg-zinc-900/95 border-2 max-h-32 overflow-x-hidden  overflow-y-scroll no-scrollbar w-full z-10 text-sm rounded-xl mt-1 flex flex-col">
+              <>
+                {defaultInterests.map(({ text }, i) => (
+                  <span
+                    onClick={handleSet}
+                    key={text + i}
+                    className={`${
+                      i === defaultInterests.length - 1 ? "" : "border-b"
+                    } px-5 py-1.5 hover:bg-zinc-800 cursor-pointer`}
+                  >
+                    {text}
+                  </span>
+                ))}
+              </>
+            </div>
+          )} */}
+              {/* <div className=" flex bottom-1.5 right-1 absolute gap-1">
+            <Button
+              onClick={() => setInterest([])}
+              size={"sm"}
+              disabled={interested.length === 0}
+              variant={"secondary"}
+              className="  border text-xs px-4 h-7 rounded-lg"
+            >
+              Remove all
+            </Button>
+            <Button
+              onClick={addInterest}
+              size={"sm"}
+              variant={"secondary"}
+              className="  border text-xs px-4 h-7 rounded-lg"
+            >
+              Add
+            </Button>
+          </div> */}
+            </div>
+          </motion.div>
           <Button
             className=" bg-zinc-900 border rounded-xl text-white hover:bg-zinc-800"
             onClick={handleContinue}
           >
             {loader ? <Loader className=" animate-spin h-5 w-5" /> : "Save"}
           </Button>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+        </DrawerHeader>
+
+        <DrawerClose
+          ref={CloseRef}
+          style={{ visibility: "hidden", position: "absolute" }}
+        ></DrawerClose>
+        {/* <DrawerClose
+            disabled={interested.length <= 1}
+            className={` bg-zinc-900 outline-none ring-0  rounded-xl hover:bg-zinc-800 ${className} text-white`}
+          >
+            Cancel
+          </DrawerClose> */}
+      </DrawerContent>
+    </Drawer>
   );
 }
 
