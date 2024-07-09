aa6f7e3100407568069ef74b7354745465c7c781
babyo77 <onlybabyyo7@gmail.com>
2024-07-09 22:13:42 +0530

    Update profile.tsx

--- src/app/discover/profile.tsx
+++ src/app/discover/profile.tsx
@@ -39,7 +39,7 @@ function Profile({
         const data = await getDiscover(mode as "pop" | "for");
         const t = setTimeout(() => {
           setProfiles(data);
-        }, profiles.length * 100);
+        }, data.length * 100);
         return () => {
           clearTimeout(t);
         };
@@ -47,7 +47,7 @@ function Profile({
         console.error(error);
       }
     },
-    [router, loggedIn, profiles]
+    [router, loggedIn]
   );
 
   return (
