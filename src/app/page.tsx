8e042b9572fd48c0ffcb87eb20c8ac223d3ca3fe
babyo77 <onlybabyyo7@gmail.com>
2024-07-09 22:04:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { getLoggedInUser } from "@/lib/server/appwrite";
 import { redirect } from "next/navigation";
 
 export default async function page() {
-  const user = await getLoggedInUser();
-
-  if (user) return redirect(`/p/${user.name}`);
-
   return redirect("/discover");
 }
