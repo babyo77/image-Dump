a1cbad65bbf1a60c1f1dac9e9880578ca06f62ec
babyo77 <onlybabyyo7@gmail.com>
2024-07-10 00:06:19 +0530

    Update profile.tsx

--- src/app/discover/profile.tsx
+++ src/app/discover/profile.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/select";
 import { getDiscover } from "@/action/getDiscover";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 function Profile({
   loggedIn,
@@ -27,6 +28,7 @@ function Profile({
 }) {
   const router = useRouter();
   const [profiles, setProfiles] = useState<discover[]>(discover);
+  const [space, setSpace] = useState<boolean>(false);
   const [isExiting, setIsExiting] = useState(false);
   const [currentMode, setCurrentMode] = useState<"pop" | "for" | null>(null);
   const [loader, setLoader] = useState<boolean>(false);
@@ -63,132 +65,191 @@ function Profile({
 
   return (
     <>
-      <header className=" w-full flex leading-tight tracking-tighter justify-between py-5 md:px-14 px-5 items-center">
-        <Link href={"/"}>
-          <motion.p
-            initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
-            animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.7,
-              stiffness: 45,
-            }}
-            exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
-            className=" font-semibold text-3xl pl-1.5 gap-0.5 flex items-center"
-          >
-            <PiStack />
-            DUMP
-          </motion.p>
-        </Link>
-        <motion.div
-          initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
-          animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            delay: 0.7,
-            stiffness: 45,
-          }}
-          exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
-          className="flex items-center gap-2"
-        >
-          <Select onValueChange={(e) => handleMode(e)}>
-            <SelectTrigger className="w-[130px] md:w-[180px]">
-              <SelectValue placeholder="Preference" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="pop">Popular</SelectItem>
-              <SelectItem value="for">For you</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <Button
-            size={"sm"}
-            onClick={() => (
-              popup.show(),
-              popup.updateMessage("A place to chill with friends and stranger.")
-            )}
-          >
-            Spaces
-          </Button>
-        </motion.div>
-      </header>
-      <AnimatePresence>
-        {loader && (
-          <motion.div
-            initial={{ filter: "blur(10px)", opacity: 0 }}
-            animate={{ filter: "blur(0px)", opacity: 1 }}
-            exit={{ filter: "blur(10px)", opacity: 0 }}
-            transition={{ duration: 0.4 }}
-            className=" w-full flex items-center justify-center h-[80dvh]"
-          >
-            <p className="font-semibold text-xl flex items-center">
-              Finding...
-            </p>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <div className="flex max-md:flex-col md:flex-wrap w-full items-center no-scrollbar h-[90dvh] overflow-y-scroll md:justify-center max-md:snap-y max-md:snap-mandatory scroll-smooth text-center px-5 text-neutral-200 pb-5 gap-4 gap-y-5">
-        <AnimatePresence onExitComplete={fetchProfiles}>
-          {!isExiting &&
-            profiles.map((user, i) => (
-              <motion.div
-                initial={{
-                  y: isDesktop ? "5dvh" : 0,
-                  opacity: 0,
-                  filter: "blur(10px)",
-                }}
-                animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+      {space ? (
+        <AnimatePresence>
+          <header className=" w-full flex leading-tight tracking-tighter justify-between py-5 md:px-14 px-5 items-center">
+            <Link href={"/"}>
+              <motion.p
+                initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+                animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
                 transition={{
                   duration: 0.5,
-                  delay: Number(`1.${i}`),
-                  type: "spring",
+                  delay: 0.7,
                   stiffness: 45,
                 }}
-                exit={{ y: isDesktop ? "5dvh" : 0, opacity: 0 }}
-                key={user.$id}
-                className={`${i === profiles.length - 1 && ""} ${
-                  i === 0 && ""
-                } relative rounded-xl max-md:snap-start scroll-smooth overflow-hidden md:w-[22dvw] md:min-h-[70dvh] min-h-[80dvh] border w-full`}
+                exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+                className=" font-semibold text-3xl pl-1.5 gap-0.5 flex items-center"
               >
-                <div
-                  className="absolute inset-0"
-                  style={{
-                    backgroundImage: `url(${user.image})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    opacity: 0.6,
-                  }}
-                ></div>
-                <div className=" absolute top-3 right-2">
-                  <Link href={`/p/${user.username}`} target="_blank">
-                    <MdOutlineArrowOutward className=" h-7 w-7" />
-                  </Link>
-                </div>
-                <div className=" absolute top-3 left-2">
-                  <Link href={`/p`} target="_blank">
-                    <Button size={"sm"}>
-                      {loggedIn ? "View your profile" : "Get your own"}
-                    </Button>
-                  </Link>
-                </div>
-                <div className=" absolute bottom-0 tracking-tight w-full text-start py-4 px-3">
-                  <p className=" text-4xl font-semibold break-words">
-                    {user.fullName}
-                  </p>
-                  <Link href={`/p/${user.username}`} target="_blank">
-                    <p className="text-sm text-neutral-300 underline underline-offset-2 ">
-                      @{user.username}
-                    </p>
-                  </Link>
+                <PiStack />
+                SPACE
+              </motion.p>
+            </Link>
 
-                  <p className=" text-base mt-0.5 text-neutral-200  font-medium break-words">
-                    {user.bio}
-                  </p>
-                </div>
+            <motion.div
+              initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+              animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                delay: 0.7,
+                stiffness: 45,
+              }}
+              exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+              className="flex items-center gap-2"
+            >
+              <div className=" h-10 w-10 rounded-full overflow-hidden">
+                <Image
+                  alt="profile-image"
+                  src={loggedIn ? loggedIn.prefs.image : "/notFound.jpg"}
+                  height={200}
+                  width={200}
+                  className=" h-[100%] w-[100%] object-cover"
+                />
+              </div>
+              <Button size={"sm"} onClick={() => setSpace(false)}>
+                Dumps
+              </Button>
+            </motion.div>
+          </header>
+          <div>
+            <AnimatePresence>
+              <motion.div
+                initial={{ filter: "blur(10px)", opacity: 0 }}
+                animate={{ filter: "blur(0px)", opacity: 1 }}
+                exit={{ filter: "blur(10px)", opacity: 0 }}
+                transition={{ duration: 0.4 }}
+                className=" w-full flex items-center justify-center h-[80dvh]"
+              >
+                <p className="font-semibold text-xl flex items-center">
+                  Coming soon
+                </p>
               </motion.div>
-            ))}
+            </AnimatePresence>
+          </div>
         </AnimatePresence>
-      </div>
+      ) : (
+        <AnimatePresence>
+          <header className=" w-full flex leading-tight tracking-tighter justify-between py-5 md:px-14 px-5 items-center">
+            <Link href={"/"}>
+              <motion.p
+                initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+                animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                  delay: 0.7,
+                  stiffness: 45,
+                }}
+                exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+                className=" font-semibold text-3xl pl-1.5 gap-0.5 flex items-center"
+              >
+                <PiStack />
+                DUMP
+              </motion.p>
+            </Link>
+            <motion.div
+              initial={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+              animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                delay: 0.7,
+                stiffness: 45,
+              }}
+              exit={{ y: 0, filter: "blur(10px)", opacity: 0 }}
+              className="flex items-center gap-2"
+            >
+              <Select onValueChange={(e) => handleMode(e)}>
+                <SelectTrigger className="w-[130px] md:w-[180px]">
+                  <SelectValue placeholder="Preference" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="pop">Popular</SelectItem>
+                  <SelectItem value="for">For you</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <Button size={"sm"} onClick={() => setSpace(true)}>
+                Spaces
+              </Button>
+            </motion.div>
+          </header>
+          <AnimatePresence>
+            {loader && (
+              <motion.div
+                initial={{ filter: "blur(10px)", opacity: 0 }}
+                animate={{ filter: "blur(0px)", opacity: 1 }}
+                exit={{ filter: "blur(10px)", opacity: 0 }}
+                transition={{ duration: 0.4 }}
+                className=" w-full flex items-center justify-center h-[80dvh]"
+              >
+                <p className="font-semibold text-xl flex items-center">
+                  Finding...
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div className="flex max-md:flex-col md:flex-wrap w-full items-center no-scrollbar h-[90dvh] overflow-y-scroll md:justify-center max-md:snap-y max-md:snap-mandatory scroll-smooth text-center px-5 text-neutral-200 pb-5 gap-4 gap-y-5">
+            <AnimatePresence onExitComplete={fetchProfiles}>
+              {!isExiting &&
+                profiles.map((user, i) => (
+                  <motion.div
+                    initial={{
+                      y: isDesktop ? "5dvh" : 0,
+                      opacity: 0,
+                      filter: "blur(10px)",
+                    }}
+                    animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+                    transition={{
+                      duration: 0.5,
+                      delay: Number(`1.${i}`),
+                      type: "spring",
+                      stiffness: 45,
+                    }}
+                    exit={{ y: isDesktop ? "5dvh" : 0, opacity: 0 }}
+                    key={user.$id}
+                    className={`${i === profiles.length - 1 && ""} ${
+                      i === 0 && ""
+                    } relative rounded-xl max-md:snap-start scroll-smooth overflow-hidden md:w-[22dvw] md:min-h-[70dvh] min-h-[80dvh] border w-full`}
+                  >
+                    <div
+                      className="absolute inset-0"
+                      style={{
+                        backgroundImage: `url(${user.image})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        opacity: 0.6,
+                      }}
+                    ></div>
+                    <div className=" absolute top-3 right-2">
+                      <Link href={`/p/${user.username}`} target="_blank">
+                        <MdOutlineArrowOutward className=" h-7 w-7" />
+                      </Link>
+                    </div>
+                    <div className=" absolute top-3 left-2">
+                      <Link href={`/p`} target="_blank">
+                        <Button size={"sm"}>
+                          {loggedIn ? "View your profile" : "Get your own"}
+                        </Button>
+                      </Link>
+                    </div>
+                    <div className=" absolute bottom-0 tracking-tight w-full text-start py-4 px-3">
+                      <p className=" text-4xl font-semibold break-words">
+                        {user.fullName}
+                      </p>
+                      <Link href={`/p/${user.username}`} target="_blank">
+                        <p className="text-sm text-neutral-300 underline underline-offset-2 ">
+                          @{user.username}
+                        </p>
+                      </Link>
+
+                      <p className=" text-base mt-0.5 text-neutral-200  font-medium break-words">
+                        {user.bio}
+                      </p>
+                    </div>
+                  </motion.div>
+                ))}
+            </AnimatePresence>
+          </div>
+        </AnimatePresence>
+      )}
     </>
   );
 }
