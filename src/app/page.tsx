ccd66479ee7f913fa82e5883c966d8fe5ec1903a
babyo77 <onlybabyyo7@gmail.com>
2024-07-09 22:22:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { getLoggedInUser } from "@/lib/server/appwrite";
 import { redirect } from "next/navigation";
 
 export default async function page() {
+  const user = await getLoggedInUser();
+
+  if (user) return redirect(`/p/${user.name}`);
+
   return redirect("/discover");
 }
