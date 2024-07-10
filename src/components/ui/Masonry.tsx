67fcd13f5777aaec6a2da341cc5723b740707176
babyo77 <onlybabyyo7@gmail.com>
2024-07-10 17:14:07 +0530

    Update Masonry.tsx

--- src/components/ui/Masonry.tsx
+++ src/components/ui/Masonry.tsx
@@ -205,6 +205,7 @@ const Masonry: React.FunctionComponent<MasonryType> = ({
                   onMouseEnter={(e) => (e.currentTarget.muted = false)}
                   onMouseLeave={(e) => (e.currentTarget.muted = true)}
                   onPlay={(e) => (e.currentTarget.poster = "")}
+                  onLoad={(e) => (e.currentTarget.poster = "")}
                   onCanPlayThrough={(e) => (
                     (e.currentTarget.controls = remove ? false : true),
                     ((e.currentTarget.poster = ""), e.currentTarget.play())
