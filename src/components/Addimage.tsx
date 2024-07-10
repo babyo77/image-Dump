bc0a970dc0d57bd2f229758fdab83472a311a2aa
babyo77 <onlybabyyo7@gmail.com>
2024-07-10 17:06:09 +0530

    Update Addimage.tsx

--- src/components/Addimage.tsx
+++ src/components/Addimage.tsx
@@ -194,10 +194,10 @@ const ImageGallery = forwardRef<HTMLButtonElement, {}>(({}, ref) => {
               toast.error("Image size exceeds 7 MB");
             }
           } else if (file.type.startsWith("video")) {
-            if (file.size <= 17 * 1024 * 1024) {
+            if (file.size <= 7 * 1024 * 1024) {
               await handleUploadHelper(file, instaLink);
             } else {
-              toast.error("Video size exceeds 17 MB");
+              toast.error("Video size exceeds 7 MB");
             }
           } else {
             toast.error("Unsupported file type");
