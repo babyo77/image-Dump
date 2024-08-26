cdb0af56fdf22f3deb0056ffe3560357c6dd105e
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-08-26 23:34:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { getLoggedInUser } from "@/action/getLogggedInUser";
 import { redirect } from "next/navigation";
 
 export default async function page() {
-  const user = await getLoggedInUser();
-
-  if (user) return redirect(`/p/${user.username}`);
-
   return redirect("/discover");
 }
